a492681bfd2120095419506b0b7d62da966b0b17
Patricio Sirena <patobsirena@gmail.com>
2025-03-06 12:01:20 -0300

    Fix: user cart page
    Modifico lógica en cart page, agrego funcion para petición en userEffect y modifico el retorno por que estaba dando warnings por las key porps

--- src/pages/UserCartPage.jsx
+++ src/pages/UserCartPage.jsx
@@ -1,19 +1,21 @@
 import { useEffect, useState } from "react"
 import TableC from "../components/TableC"
 import clienteAxios, { configHeaders } from "../helpers/axios"
+import { Button } from "react-bootstrap"
 
 
 const UserCartPage = () => {
 
     const [cartProducts, setCartProducts] = useState([])
+    const [totalPrice, setTotalPrice] = useState(0)
     const [isLoading, setIsLoading] = useState(true)
-    
-    const getCart = async () =>{
+
+    const getCart = async () => {
         const arrayCart = [];
         const result = await clienteAxios.get('/products/getCart', configHeaders)
         const cartResult = result.data.products
-        for(const item of cartResult){
-            const {idProduct, quantity} = item
+        for (const item of cartResult) {
+            const { idProduct, quantity } = item
             try {
                 const response = await clienteAxios.get(`/products/${idProduct}`, configHeaders)
                 const product = response.data
@@ -22,22 +24,44 @@ const UserCartPage = () => {
             } catch (error) {
                 console.log(error);
             }
-        }        
+        }
         setCartProducts(arrayCart)
         setIsLoading(false)
     }
 
+    const handleClickMpPay = async () => {
+        try {
+            const response = await clienteAxios.post('/products/mpPayment', {},configHeaders)
+            location.href = response.data.mpLink
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
     useEffect(() => {
-        if(isLoading){
+        if (isLoading) {
             getCart()
         }
     }, [isLoading])
-    
 
+    useEffect(() => {
+        const getTtotalPrice = () => {
+            let cartPrice = 0
+            for (const product of cartProducts) {
+                cartPrice += product.price * product.quantity
+            }
+            setTotalPrice(cartPrice)
+        }
+        getTtotalPrice()
+    }, [cartProducts])
 
     return (
         <>
-        <TableC idPage={'userCart'} array={cartProducts} setIsLoadingHook={setIsLoading}/>
+            <TableC idPage={'userCart'} array={cartProducts} setIsLoadingHook={setIsLoading} />
+            <div className="priceAndConfirm" style={{ margin: '2em 10em', textAlign: 'end' }}>
+                <p>Total: ${totalPrice}</p>
+                <Button onClick={handleClickMpPay}>Confirmar compra</Button>
+            </div>
         </>
     )
 }
--- src/pages/UserOrdersPage.jsx
+++ src/pages/UserOrdersPage.jsx
@@ -1,43 +1,11 @@
-import { useEffect, useState } from "react"
+import React, { useEffect, useState } from "react"
 import clienteAxios, { configHeaders } from "../helpers/axios"
 import { Button, Col, Container, Row } from "react-bootstrap"
-import { Link, useNavigate } from "react-router-dom"
+import { Link } from "react-router-dom"
 
 const UserOrdersPage = () => {
     const [orders, setOrders] = useState([])
     const [isLoading, setIsLoading] = useState(true)
-    const navigate = useNavigate()
-    const token = JSON.parse(sessionStorage.getItem('token'))
-
-    const getOrders = async () => {
-        try {
-            const result = await clienteAxios.get('/products/getOrders', configHeaders)
-            const ordersArray = result.data;
-            const updatedOrders = []
-            for (const item of ordersArray) {
-                const { products, date, ...order } = item
-                const newDate = convertDate(date)
-                order.date = newDate
-                order.items = []
-                for (const element of products) {
-                    const result = await clienteAxios.get(`/products/${element.idProduct}`)
-                    if (result.status === 200) {
-                        const product = result.data
-                        product.quantity = element.quantity
-                        order.items.push(product)
-                    } else {
-                        const product = { _id: element.idProduct, quantity: element.quantity }
-                        order.items.push(product)
-                    }
-                }
-                updatedOrders.push(order)
-                setOrders(updatedOrders)
-            }
-            setIsLoading(false)
-        } catch (error) {
-            console.log(error)
-        }
-    }
 
     const convertDate = (date) => {
         const options = { year: 'numeric', month: 'long', day: 'numeric' }
@@ -46,32 +14,54 @@ const UserOrdersPage = () => {
     }
 
     const handleClickAddToCart = async (productId) => {
-        if (token === null) {
-            alert('Debe iniciar sesion, redirigiendo a login')
-            setTimeout(() => {
-                navigate('/login')
-            }, 1000);
-        }
         try {
             const result = await clienteAxios.post(`/products/addToCart/${productId}`, {}, configHeaders)
             alert(result.data.msg);
-            setIsLoading(true)
         } catch (error) {
             alert(error.response.data.msg);
         }
     }
 
     useEffect(() => {
+        const getOrders = async () => {
+            try {
+                const result = await clienteAxios.get('/products/getOrders', configHeaders)
+                const ordersArray = result.data;
+                const updatedOrders = []
+                for (const item of ordersArray) {
+                    const { products, date, ...order } = item
+                    const newDate = convertDate(date)
+                    order.date = newDate
+                    order.items = []
+                    for (const element of products) {
+                        const result = await clienteAxios.get(`/products/${element.idProduct}`)
+                        if (result.status === 200) {
+                            const product = result.data
+                            product.quantity = element.quantity
+                            order.items.push(product)
+                        } else {
+                            const product = { _id: element.idProduct, quantity: element.quantity }
+                            order.items.push(product)
+                        }
+                    }
+                    updatedOrders.push(order)
+                }
+                setOrders(updatedOrders)
+                setIsLoading(false)
+            } catch (error) {
+                console.log(error)
+            }
+        }
         if (isLoading) {
             getOrders()
         }
-    },)
+    }, [isLoading])
 
     return (
         <Container>
             {orders.map((order) =>
-                <>
-                    <Row key={order._id} className="cardOrder" style={{ width: '100%', minHeight: '8em', border: '1px solid grey', margin: '1em 0', borderRadius: '5px' }}>
+                <React.Fragment key={order._id}>
+                    <Row className="cardOrder" style={{ width: '100%', minHeight: '8em', border: '1px solid grey', margin: '1em 0', borderRadius: '5px' }}>
                         <Col sm={12} md={12} lg={12} style={{ height: '1.5em', margin: '.5em 0' }}>{order.date}</Col>
                         <hr style={{ margin: '0', padding: '0', color: 'black' }} />
                         {
@@ -96,7 +86,7 @@ const UserOrdersPage = () => {
                         order.paymentStatus === 'pending' &&
                         <Button variant="primary">Pagar</Button>
                     }
-                </>
+                </React.Fragment>
             )}
         </Container>
     )
